4109cab1c4cc61806c25f880679caf05395cec54
Dutt Mistry <dutta.mistry@gmail.com>
2023-09-08 18:49:10 +0530

    Changes

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,20 +2,35 @@ import { Button, Container, Grid, TextField, Typography } from "@mui/material";
 import { useFormik } from "formik";
 import React, { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
-
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
 function Welcome() {
-    const { connectWallet } = useContext(TransactionContext);
-  const { values, errors, handleChange, handleSubmit, handleBlur } = useFormik({
-    initialValues: {
-      addressTo: "",
-      amount: "",
-      keyword: "",
-      message: "",
-    },
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+    const {
+      connectWallet,
+      currentAccount,
+      formData,
+      handleChange,
+      sendTransaction,
+    } = useContext(TransactionContext);
+ 
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+
+    e.preventDefault();
+
+    if (!addressTo || !amount || !keyword || !message) return;
+
+    sendTransaction();
+  };
+
 
 
   return (
@@ -36,11 +51,13 @@ function Welcome() {
             <Grid item xs={4} sm={8} md={12}>
               <Typography variant="h6">Send Crypto Across The World</Typography>
             </Grid>
-            <Grid item xs={4} sm={8} md={12}>
-              <Button variant="contained" onClick={connectWallet}>
-                Connect Wallet
-              </Button>
-            </Grid>
+            {!currentAccount && (
+              <Grid item xs={4} sm={8} md={12}>
+                <Button variant="contained" onClick={connectWallet}>
+                  Connect Wallet
+                </Button>
+              </Grid>
+            )}
             <Grid item xs={4} sm={8} md={12}>
               <div>
                 <div
@@ -100,47 +117,33 @@ function Welcome() {
           </Grid>
         </Grid>
         <Grid item xs={4} sm={4} md={3}>
-          <form onSubmit={handleSubmit}>
-            <TextField
+            <Input
+              placeholder="Address To"
               name="addressTo"
-              value={values?.addressTo}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              id="outlined-basic"
-              label="Address To"
-              variant="outlined"
+              type="text"
+              handleChange={handleChange}
             />
-            <TextField
+            <Input
+              placeholder="Amount (ETH)"
               name="amount"
-              value={values?.amount}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              id="outlined-basic"
-              label="Amount (ETH)"
-              variant="outlined"
+              type="number"
+              handleChange={handleChange}
             />
-            <TextField
+            <Input
+              placeholder="Keyword (Gif)"
               name="keyword"
-              value={values?.keyword}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              id="outlined-basic"
-              label="Keyword"
-              variant="outlined"
+              type="text"
+              handleChange={handleChange}
             />
-            <TextField
+            <Input
+              placeholder="Enter Message"
               name="message"
-              value={values?.message}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              id="outlined-basic"
-              label="Message"
-              variant="outlined"
+              type="text"
+              handleChange={handleChange}
             />
-            <Button variant="contained" onClick={handleSubmit}>
+            <button onClick={handleSubmit}>
               Send Now
-            </Button>
-          </form>
+            </button>
         </Grid>
       </Grid>
     </Container>
--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -1,14 +1,14 @@
 import { SettingsEthernetSharp } from '@mui/icons-material';
 import React, { useEffect, useState } from 'react'
 import { contractABI, contractAddress } from '../utils/constants';
-import {ethers} from 'ethers';
-
+// import {ethers} from 'ethers';
+const ethers = require("ethers");
 
 export const TransactionContext = React.createContext();
 
 const {ethereum} = window;
 
-const getEthereumContext = ()=>{
+const getEthereumContract = ()=>{
     const provider =new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(
@@ -21,31 +21,68 @@ console.log({
     provider,signer,transactionContract
 })
 }
+export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [formData, setFormData] = useState({
+    addressTo: "",
+    amount: "",
+    keyword: "",
+    message: "",
+  });
+  const handleChange = (e, name) => {
+    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
+  };
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (!ethereum) return alert("please Install Metamask");
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts?.length) {
+        setCurrentAccount(accounts[0]);
+        //getAllTransactions();
+      } else {
+        console.log("NO Accounts Found.");
+      }
+      console.log(accounts);
+    } catch (error) {
+      console.log("error: ", error);
+      throw new Error("No Ethereum Object.");
+    }
+  };
 
-export const TransactionProvider = ({children})=>{
-    const [connectedAccount,setConnectedAccount] = useState("")
-    const checkIfWalletIsConnected = async ()=>{
-        if(!ethereum) return alert("please Install Metamask");
-    const accounts = await ethereum.request({method:"eth_accounts"})
-    console.log(accounts)
+  const sendTransaction = async () => {
+    try {
+      if (!ethereum) return alert("please Install Metamask");
+      const { addressTo, amount, keyword, message } = formData;
+      getEthereumContract();
+    } catch (error) {}
+  };
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  });
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("please Install Metamask");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log("error: ", error);
+      throw new Error("No Ethereum Object.");
     }
-    useEffect(()=>{
-        checkIfWalletIsConnected();
-    })
-    const connectWallet = async ()=>{
-        try {
-            if(!ethereum) return alert("please Install Metamask");
-            const accounts = await ethereum.request({method:"eth_requestAccounts"})
-            setConnectedAccount(accounts[0]);
-        } catch (error) {
-            console.log('error: ', error);
-            throw new Error("No Ethereum Object.")
-            
-        }
-    }
-    return (
-        <TransactionContext.Provider value={{connectWallet}}>
-            {children}
-        </TransactionContext.Provider>
-    )
-}
+  };
+  return (
+    <TransactionContext.Provider
+      value={{
+        connectWallet,
+        currentAccount,
+        formData,
+        setFormData,
+        handleChange,
+        sendTransaction,
+      }}
+    >
+      {children}
+    </TransactionContext.Provider>
+  );
+};
